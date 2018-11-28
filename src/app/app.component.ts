70e492f601ca04caa3ed6428d77a2033b4bb89d6
Katya Maksymenko <6aff14@gmail.com>
2018-11-28 19:51:48 +0200

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,25 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'My second Angular App';
   comment = {
     name: '',
-    message: '',
-    delete: ''
+    message: ''
   };
 
   comments = [];
 
-    constructor() {
-      this.comments = JSON.parse(localStorage.getItem('comments'));
-    }
+  constructor() {
+    this.comments = JSON.parse(localStorage.getItem('comments'));
+  }
 
   send(): void {
-    this.comments.push({ name: this.comment.name, message: this.comment.message, delete: this.comment.delete });
+    this.comments.push({ name: this.comment.name, message: this.comment.message });
     localStorage.setItem('comments', JSON.stringify(this.comments));
   }
 
-    out(index): void {
-        this.comments.splice(index, 1);
-        localStorage.setItem('comments', JSON.stringify(this.comments));
-    }
+  delete(index): void {
+      this.comments.splice(index, 1);
+      localStorage.setItem('comments', JSON.stringify(this.comments));
+  }
 
 }
